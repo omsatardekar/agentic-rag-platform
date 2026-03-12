36160b4cb029e9f4867b6b3d14ff877dc2f77ba5
omkar satardekar <omkarsatardekar4002@gmail.com>
2026-03-12 22:05:08 +0530

    feat: make google client id configurable

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { AuthProvider } from './context/AuthContext'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
 // You must paste your Client ID here between the quotes
-const GOOGLE_CLIENT_ID = "405165363884-4rn5umomij7hcslcika2gkgl8ofbq49k.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "405165363884-4rn5umomij7hcslcika2gkgl8ofbq49k.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
